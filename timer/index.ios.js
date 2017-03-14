c5491fc916e2e56e30bd7e8b422e9e1c9bfa75d9
Ben Ellentuck <ellentuckben@gmail.com>
2017-03-14 19:29:32 -0400

    changing project name from GrabBag to timer, which is tricky in RN and may just warrant putting a copy of relevant things into a new react-native-init directory...we shall see

--- timer/index.ios.js
+++ timer/index.ios.js
@@ -3,4 +3,4 @@ import {
 } from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('GrabBag', () => App);
+AppRegistry.registerComponent('timer', () => App);
--- timer/ios/GrabBagTests/GrabBagTests.m
+++ timer/ios/GrabBagTests/GrabBagTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface GrabBagTests : XCTestCase
+@interface timerTests : XCTestCase
 
 @end
 
-@implementation GrabBagTests
+@implementation timerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
