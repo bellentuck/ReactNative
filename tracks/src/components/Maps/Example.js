d6abbe552eb8784e225d8e2ef15bfcddfd5dac8b
Ben Ellentuck <ellentuckben@gmail.com>
2017-04-07 23:39:14 -0400

    Starting to think about maps and coming to appreciate the capabilities of physics engines...different React Native project coming soon...but this one at this point, even as proof-of-concept, really could benefit from a more systematically game-oriented frame - as meantime art and science move toward Unity

--- tracks/src/components/Maps/Example.js
+++ tracks/src/components/Maps/Example.js
@@ -4,4 +4,64 @@
 - return: the corresponding view of the actual track pieces themselves.
 THEN, in "App", you can load a map.
 and then, when you add or remove tracks, youre *really* making changes to the map is what youre doing.
+
+eventually, this is to be a db. - firebase?
 */
+
+import React, { Component } from 'react';
+
+class Example extends Component {
+
+  addTrackToMap()
+  addCarToMap()
+  activateTrack()
+
+  render() {
+    const map = {
+      tracks: {
+        piece1: {
+          type: 'CurvePiece',
+          origin: {x: 260, y: 244},
+          height: 60, //this._radius = height+5
+          length: 60,
+          rotation: 270
+        },
+        piece2: {
+          type: 'StraightPiece',
+          origin: {x: 160, y: 144},
+          length: 80,
+          rotation: 0
+        }
+      },
+      trains: {
+        train1: {
+          type: 'SubwayCar',
+          car1: {
+            frontWheels: {
+              x1: ,//xStart
+              y1: ,//yStart-2(?)
+              x2: ,//xStart+8.60 //SubwayCar-specific width
+              y2: //yStart-2(?)
+            },
+            backWheels: {
+              x1: ,//xStart
+              y1: ,//yStart-height+2
+              x2: ,//xStart+8.60
+              y2: //yStart-height+2
+            }
+          }
+        }
+      }
+    }
+    return (
+      <Svg height="750" width="450">
+      for (let id in tracks) { }
+      <map.tracks.piece1
+      <CurvePiece origin={{x: 260, y: 244}} height={this._radius-5} length={this._radius-5} rotation={270} />
+      <StraightPiece origin={{x: 160, y: 144}} length={80} rotation={0} />
+      </Svg>
+    );
+  }
+}
+
+export default Example;
